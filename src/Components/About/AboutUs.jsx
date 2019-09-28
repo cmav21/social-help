f85e1467e5819cd976c7229b6bc0bd09919ba6ee
cesar magallon <cesar9mv@gmail.com>
2019-09-28 01:52:43 -0500

    [fix]text of about us

--- src/Components/About/AboutUs.jsx
+++ src/Components/About/AboutUs.jsx
@@ -10,30 +10,28 @@ const AboutUs = () => (
     <Container fluid={true} className="aboutUs p-0">
         <Container>
             <Row>
-                <Jumbotron className="bg-light mb-0">
+                <Jumbotron className="bg-white mb-0">
                     <h1>Somos social Help</h1>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates laborum iusto et in temporibus officiis fugiat earum repudiandae quis totam assumenda perspiciatis voluptatum, tenetur ullam recusandae quos possimus blanditiis.
                     </p>
                 </Jumbotron>
-                <Jumbotron className="bg-light mb-0">
+                <Jumbotron className="bg-white mb-0">
                     <h1>Nuestros valores</h1>
                     <hr/>
                         <ul style={{width:'100%'}}>
-                            <li className="text-left">Lorem ipsum dolor sit.</li>
-                            <li className="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, porro.</li>
-                            <li className="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iusto reprehenderit adipisci obcaecati voluptate a quod aliquam perferendis dolores rem doloribus non vel, maxime blanditiis dicta? Ratione </li>
-                            <li className="text-left">Lorem ipsum dolor sit amet.</li>
-                            <li className="text-left">Lorem ipsum dolor, suscipit molestias exercitationem qui voluptatum ullam enim. Ut, nesciunt!</li>
-                            <li className="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                            <li className="text-left">Lorem ipsum dolor sit.</li>
+                            <li className="text-left">Responsabilidad: Cumplir con los tiempos de entrega previamente acordados y cubrir los estándares de calidad.</li>
+                            <li className="text-left">Respeto: Una comunic ación sana entre los miembros del equipo de trabajo y los clientes de la empresa.</li>
+                            <li className="text-left">Tolerancia: Poder resolver las diferencias que pudieran existir entre los integrantes que conforman la empresa.</li>
+                            <li className="text-left">Dedicación: Realizar el esfuerzo máximo en cada una de nuestras tareas.</li>
+                            <li className="text-left">Los miembros del equipo de trabajo estarán estrictamente apegados a las normas de trabajo y valores establecidos por la empresa.</li>
                         </ul>
                 </Jumbotron>
-                <Jumbotron className="bg-light">
+                <Jumbotron className="bg-white">
                     <h1>Nuestra misión</h1>
                     <hr/>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sed et quo. Consectetur voluptas distinctio iste eos ducimus facere expedita alias. Cum nisi quaerat nostrum totam maiores animi, tempore eos.
+                    <p className="text-center">
+                        Desarrollar software personalizado para dar solución a las posibles problemáticas de nuestros clientes y/o mejorar su trabajo.
                     </p>
                 </Jumbotron>
             </Row>
